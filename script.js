143f8a180610b380ea2b6f26258a4e5ec16125b2
Zena Anthony <85813190+zena-a@users.noreply.github.com>
2022-05-16 18:19:01 -0400

    Updated images to root folder source

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ var randomNumber1 = Math.floor(Math.random() * 6) + 1;
 var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
 //Changes dice image 
-document.querySelector("img").setAttribute("src", "images/dice" + randomNumber1 + ".png");
-document.querySelector("img.img2").setAttribute("src", "images/dice" + randomNumber2 + ".png");
+document.querySelector("img").setAttribute("src", "/images/dice" + randomNumber1 + ".png");
+document.querySelector("img.img2").setAttribute("src", "/images/dice" + randomNumber2 + ".png");
 
 //Shows which player is the winner, or if it is a draw
 function diceWinner(player1, player2) {
